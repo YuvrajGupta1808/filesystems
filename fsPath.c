7b79ec41fc01ffa73fe67ca77d5415e2e6435eb7
sulavjung <sulavjung2000@icloud.com>
2024-07-28 14:02:02 -0700

    Changes:  and Added comments to explain each logic of each method Added comments to explain each logic of each methods. Organize the method flow for better understanding of the code.

--- fsPath.c
+++ fsPath.c
@@ -1,3 +1,22 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2024
+* Name:: Yuvraj Gupta, Fasika Abera, Sulav Jung Hamal, Miguel Maurer
+* Student IDs:: 922933190, 923038932, 923075813, 922097199
+* GitHub-Name:: YuvrajGupta1808, Fasikaabera, Sulavjung, miguelCmaurer
+* Group-Name:: Satisfaction
+* Project:: Basic File System
+*
+* File:: fsPath.c
+*
+* Description:: This file implements a stack-based path management system. 
+* It includes functions to manage the stack, clean and parse paths, and 
+* convert the stack to a string representation of the path. The implementation
+* ensures memory is properly allocated and freed, and paths are correctly 
+* parsed and managed.
+*
+**************************************************************/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,7 +28,7 @@ char* stack[MAXSTACKSIZE];
 char* strPath = NULL;
 int size = 0;
 
-// adds new value to top of stack
+//Adds new value to the top of the stack. 
 void push(char* value){
     // allocate memory for str
     stack[size] = malloc(strlen(value) + 1);
@@ -22,6 +41,7 @@ void push(char* value){
     size++;
 }
 
+//Removes the top value from the stack. 
 void pop(){
     // decrement size
     size--;
@@ -31,38 +51,8 @@ void pop(){
     stack[size] = NULL;
 }
 
-char* toString(){
-    int strLen = 0;
-    int offset = 0;
-    // itterate over stack and its values to get amount of memory needed
-    for(int i = 0; i < size; i++){
-        strLen += strlen(stack[i])+1;
-    }
-    // if strPath is not null free it
-    if (strPath != NULL) {
-        free(strPath);
-        strPath = NULL;
-    }
-    // allocate the memory
-    char *newStrPath = realloc(strPath, strLen + 2);
-    if (newStrPath == NULL) {
-        return NULL;
-    }
-    // set strPath to newStrPath
-    strPath = newStrPath;
-    strPath[0] = '\0';
-    
-    // iterate over size concatinating string
-    for(int i = 0; i < size; i++){
-        strcat(strPath, "/");
-        strcat(strPath, stack[i]);
-    }
-    // add final '/'
-    strcat(strPath, "/");
-    return strPath;
-}
 
-
+//Cleans and updates the path based on the provided path string. 
 void pathCleaner(char* path){
     // if path is root remove all of old path
     if( path[0] == '/' ){
@@ -95,6 +85,7 @@ void pathCleaner(char* path){
     
 }
 
+// Parses the path and updates the ppinfo structure. 
 int parsePath(char* path, ppinfo* ppi){
     // if path is not valid return error
     if(path == NULL){
@@ -155,19 +146,57 @@ int parsePath(char* path, ppinfo* ppi){
     
 }
 
+//Conversts the stack to a single path string. 
+char* toString(){
+    int strLen = 0;
+    int offset = 0;
+    // Iterate over stack and its values to get amount of memory needed
+    for(int i = 0; i < size; i++){
+        strLen += strlen(stack[i])+1;
+    }
+    // if strPath is not null, free it
+    if (strPath != NULL) {
+        free(strPath);
+        strPath = NULL;
+    }
+    // allocate the memory
+    char *newStrPath = realloc(strPath, strLen + 2);
+    if (newStrPath == NULL) {
+        return NULL;
+    }
+    // set strPath to newStrPath
+    strPath = newStrPath;
+    strPath[0] = '\0';
+    
+    // iterate over size concatinating string
+    for(int i = 0; i < size; i++){
+        strcat(strPath, "/");
+        strcat(strPath, stack[i]);
+    }
+    // add final '/'
+    strcat(strPath, "/");
+    return strPath;
+}
+
+// Sets the current working directory string. 
 void setCWDStr(char *pathCpy){
     pathCleaner(pathCpy);
 }
 
+//Gets the current working directory string. 
 char* getCWDStr(){
     return toString();
 }
 
+
+//Frees the ppinfo structure. 
 void freePPI(ppinfo* ppi){
     freeIfNotNeeded(ppi->parent);
     free(ppi);
 }
 
+
+// Frees the current working directory string and stack. 
 void freeSTRCWD(){
     
     while(size > 0 ){
